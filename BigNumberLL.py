33bbe4b42bc786d0e1ae72f7eedfd3c2b36bb50d
Oori Schubert <oorischubert@gmail.com>
2022-10-18 21:06:32 -0400

    add sub done

--- BigNumberLL.py
+++ BigNumberLL.py
@@ -6,40 +6,26 @@
 class Link:
     def __init__(self, data):
         self.data = data
-        self.prev = None
-        self.next = None
+        self.prev = None   
+        self.next = None   
 
     def __str__(self):
       return str(self.data)
-
-#The stack is displayed in reverse order (top at the bottom).
-# The constructor of BigNumberLL accepts one input String argument that may contain a negative sign (first character).
-#When the big number is displayed, the number of digits that composed the number (i.e. size of the Linked-List) is also displayed at first between square brackets.
-#If the user enters meaningless zero on the left, they are removed from the Link-list before displaying.
-#To ease the reading of the big number, a comma “,” is placed every three digits. Basic methods to implement
-#insertLast: If you read a String, character by character from left to right, you need to be able to insert each integer (converted from the character) inside the linked-list while keeping the ordering of a queue. For example: 3,458, do insertLast(3), insertLast(4), etc. This method can be used inside your constructor.
-#insertFirst: reverse the order of insertion. This method is not needed for this preliminary part of the project but it is a kernel method which is needed by the next tasks. While doing an operation (for example addition), you often perform it step by step from the right to the left. The resulting integer needs to be inserted in the list using insertFirst to make sure that the ordering of the final number is correct.
-#deleteFirst: it is used by the trimFront method.
-#trimFront: if the BigNumber list contains a lot of zeros on the left, you can trim this number
-#(remove the zeros) and reduce its size. This method can be used inside your constructor.
-# str : Convert the big number list into a string that can be used to print by the Stack. A negative sign must be added at the beginning of the String if the number is negative (i.e. ’positive’ flag equal to False). The size of the linked-list is also displayed at first between square brackets. For full credit, you will also include a comma “,” to separate all the thousands. For example: 3458 is “3,458”; 345 is “345”, 12349875 is “12,349,875”. Hint: various techniques possible. You could scan the linked-list from right to left, keep track of the number of digits, and check the remainder while dividing by 3.
-#All following Tasks 1, 2, and 3 can be implemented in any order. Task 4 and 5 should be the last two you implement.
-
-
   
 class BigNumberLL: ### To complete
     
-    def __init__(self, number):
+    def __init__(self, number=None):
         self.first = None
         self.last = None
         self.size = 0
         self.positive=True
-        if number[0]=="-":
-          self.positive=False
-          number=number[1:]
-        for i in range(len(number)):
-            self.insertLast(int(number[i]))
-        self.trimFront()
+        if number is not None:
+            if number[0]=="-":
+                self.positive=False
+                number=number[1:] #skip negative sign
+            for i in range(len(number)):
+                self.insertLast(int(number[i]))
+            self.trimFront()
     
     def insertLast(self, data):
         if self.first == None:
@@ -48,7 +34,7 @@ class BigNumberLL: ### To complete
         else:
             newLink = Link(data)
             self.last.next = newLink
-            newLink.prev = self.last
+            newLink.prev = self.last 
             self.last = newLink
         self.size += 1
     
@@ -63,35 +49,144 @@ class BigNumberLL: ### To complete
             self.first = newLink
         self.size += 1
     
+    #Delete from double linked list if 0
     def deleteFirst(self):
         if self.first == None:
             return None
         else:
             temp = self.first
             self.first = self.first.next
+            self.first.prev = None
             self.size -= 1
-            return temp.data
     
-    def trimFront(self):
-        while self.first.data == 0:
+    def trimFront(self) -> None:
+        while self.first.data == 0 and self.size is not 1:
             self.deleteFirst()
-    
+            
     def __str__(self):
-        display = ""
-        display += "["
-        display += str(self.size)
-        display += "] "
+        display = "[%s] "%(self.size)
+        numDisplay = ""
         if not self.positive:
             display += "-"
-        current = self.last
+        current = self.last #start at end
         count = 0
         while current != None:
-            display += str(current.data)
+            numDisplay += str(current.data)
             count += 1
-            if count % 3 == 0:
-                display += ","
-            current = current.prev
-        return display
+            if count % 3 == 0 and current.prev is not None:
+                numDisplay += ","
+            current = current.prev #go backwards
+        newNumDisplay = ""
+        for i in range(len(numDisplay)): #flip the number to corretly show
+            newNumDisplay += numDisplay[len(numDisplay)-i-1]
+        return display + newNumDisplay
+    
+    def __gt__(self,y):
+        #Scan size of each 
+        if self.positive and not y.positive: return True
+        if not self.positive and y.positive: return False
+        if self.positive:
+            if self.size > y.size: return True
+            elif self.size < y.size: return False
+        if not self.positive:
+            if self.size < y.size: return True
+            elif self.size > y.size: return False
+        else: 
+            current = self.first
+            yCurrent = y.first
+            while current is not None:
+                if self.positive: 
+                    if current.data > yCurrent.data: return True
+                elif not self.positive:
+                  if current.data < yCurrent.data: return True
+                current = current.next
+                yCurrent = yCurrent.next
+            return False
+
+    def __lt__(self,y):
+        return(y>self)
+
+    def __eq__(self,y):
+        if  self>y or y>self: return False
+        else: return True 
+        
+    def __add__(self,y):
+        if self.size > y.size:
+            biggerN = self
+            smallerNum = y
+        else: #order doesnt matter if they are equal size
+            biggerN = y
+            smallerNum = self
+        bigCurrent = biggerN.last
+        smallCurrent = smallerNum.last
+        newNum = BigNumberLL()
+        remainder = 0
+        while bigCurrent is not None:
+            bigData = bigCurrent.data
+            if smallCurrent is None:
+                smallData = 0
+            else: 
+                smallData = smallCurrent.data
+                smallCurrent = smallCurrent.prev
+            newData = bigData + smallData + remainder #add remainder to new row
+            if newData >= 10:
+                remainder = int(str(newData)[0])
+                newNum.insertFirst(int(str(newData)[1]))
+            else:
+                remainder = 0
+                newNum.insertFirst(newData)
+            bigCurrent = bigCurrent.prev
+        if remainder is not 0:
+            newNum.insertFirst(remainder)
+        return newNum
+
+    def __sub__(self,y) -> "BigNumberLL":
+        if self>y:
+            biggerN = self
+            smallerNum = y
+        else: #order doesnt matter if they are equal size
+            biggerN = y
+            smallerNum = self
+        bigCurrent = biggerN.last
+        smallCurrent = smallerNum.last
+        newNum = BigNumberLL()
+        if self<y: #make sign negative if what your subtracting by is larger
+            newNum.positive = False
+        remainder = 0
+        while bigCurrent is not None:
+            bigData = bigCurrent.data
+            if smallCurrent is None:
+                smallData = 0
+            else: 
+                smallData = smallCurrent.data
+                smallCurrent = smallCurrent.prev
+            newData = bigData - smallData + remainder #add remainder to new row
+            remainder = 0
+            if newData < 0:
+                newData = 10 + newData
+                remainder -=1
+            if bigData == 0 and not smallData == 0:
+                remainder -=1
+            newNum.insertFirst(newData)
+            bigCurrent = bigCurrent.prev
+        newNum.trimFront()
+        return newNum
+
+
+
+
+        
+
+
+        
+
+
+
+        
+
+
+
+    
     
  
 
